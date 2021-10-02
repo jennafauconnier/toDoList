b740e4a698684489eb7e49e43c004f6fe9c0a483
Jenna <jenna.fauconnier@gmail.com>
2021-10-02 16:26:07 +0200

    add filter function for button

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const toDoInput = document.querySelector(".todo-input");
 const toDoButton = document.querySelector(".todo-button");
 const toDoList = document.querySelector(".todo-list");
+const filterOption = document.querySelector(".filter-todo");
 
 toDoButton.addEventListener("click", addToDo);
+toDoList.addEventListener("click", deleteCheck);
+filterOption.addEventListener("input", filterToDo);
 
 function addToDo(event) {
     event.preventDefault();
@@ -27,6 +30,33 @@ function addToDo(event) {
     // Ajouter todo a la todo-list
     toDoList.appendChild(toDoDiv);
     toDoInput.value = "";
+}
 
+function deleteCheck(e) {
+    const item = e.target;
+    // Delete TODO
+    if (item.classList[0] === "trash-btn") {
+        const todo =  item.parentElement;
+        todo.classList.add("fall")
+        todo.addEventListener("transitionend", function(){
+            todo.remove();
+        });
+    }
+    // Check mark
+    if (item.classList[0] === "complete-btn") {
+        const todo = item.parentElement;
+        todo.classList.toggle("completed");
+    }
+}
+
+function filterToDo(event) {
+    const todos = toDoList.childNodes;
+    todos.forEach(function(todo) {
+        switch(e.target.value) {
+            case "all":
+                todo.style.display = "flex";
+                break;
+        }
+    })
 }
 
